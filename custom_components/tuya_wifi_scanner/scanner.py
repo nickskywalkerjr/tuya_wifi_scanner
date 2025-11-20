712739d6bfd8b12be5cea619f23ebbedb299db9e
nickskywalkerjr <118694010+nickskywalkerjr@users.noreply.github.com>
2025-11-19 22:01:24 -0500

    Implement device key validation function
    Added a function to validate device keys for Tuya devices.

--- custom_components/tuya_wifi_scanner/scanner.py
+++ custom_components/tuya_wifi_scanner/scanner.py
@@ -7,3 +7,15 @@ async def discover_tuya_devices(hass: HomeAssistant):
     return await hass.async_add_executor_job(
         tinytuya.deviceScan, DISCOVERY_TIMEOUT, True
     )
+
+def validate_device_key(device_id, ip, key):
+    """Return True if we can connect & decrypt with the provided key."""
+
+    d = tinytuya.Device(device_id, ip, key)
+    d.set_version(3.3)
+
+    try:
+        data = d.status()   # Try querying the device
+        return data is not None and "dps" in data
+    except Exception:
+        return False
