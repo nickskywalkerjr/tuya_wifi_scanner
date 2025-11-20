990f2244e304bf3eb8e77f3ff649e5a03cca9084
nickskywalkerjr <118694010+nickskywalkerjr@users.noreply.github.com>
2025-11-19 19:52:06 -0500

    Update __init__.py

--- custom_components/tuya_wifi_scanner/__init__.py
+++ custom_components/tuya_wifi_scanner/__init__.py
@@ -1,1 +1,20 @@
+from .const import DOMAIN
+
+async def async_setup_entry(hass, entry):
+    """Set up integration from Config Entry."""
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = {
+        "device_id": entry.data.get("device_id"),
+        "device_ip": entry.data.get("device_ip"),
+        "device_key": entry.data.get("device_key"),
+    }
+
+    return True
+
+
+async def async_unload_entry(hass, entry):
+    """Unload a config entry."""
+    hass.data[DOMAIN].pop(entry.entry_id, None)
+    return True
 
