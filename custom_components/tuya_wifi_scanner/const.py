e5ae02c1dc6cae03fa8447186f5fea0b56c5fb73
nickskywalkerjr <118694010+nickskywalkerjr@users.noreply.github.com>
2025-11-19 22:17:22 -0500

    Update const.py

--- custom_components/tuya_wifi_scanner/const.py
+++ custom_components/tuya_wifi_scanner/const.py
@@ -1,2 +1,10 @@
 DOMAIN = "tuya_wifi_scanner"
+
+# seconds to wait for LAN discovery
 DISCOVERY_TIMEOUT = 5
+
+# optional future constants
+DEFAULT_DEVICE_VERSION = 3.3
+CONF_DEVICE_ID = "device_id"
+CONF_DEVICE_IP = "device_ip"
+CONF_DEVICE_KEY = "device_key"
