42c53c7bc1c628b2534ec9727df7a4ab1c36fc99
nickskywalkerjr <118694010+nickskywalkerjr@users.noreply.github.com>
2025-11-19 22:51:40 -0500

    Refactor constants in const.py

--- custom_components/tuya_wifi_scanner/const.py
+++ custom_components/tuya_wifi_scanner/const.py
@@ -1,10 +1,4 @@
+"""Constants for Tuya WiFi Scanner integration."""
+
 DOMAIN = "tuya_wifi_scanner"
-
-# seconds to wait for LAN discovery
-DISCOVERY_TIMEOUT = 5
-
-# optional future constants
-DEFAULT_DEVICE_VERSION = 3.3
-CONF_DEVICE_ID = "device_id"
-CONF_DEVICE_IP = "device_ip"
-CONF_DEVICE_KEY = "device_key"
+DISCOVERY_TIMEOUT = 5  # seconds
